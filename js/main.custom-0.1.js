b99cef9e3c265b51561cea243560de784925eede
Warren Leyes <warren.leyes@gmail.com>
2018-02-20 13:14:46 -0800

    add cog icon for settings. Roughed-out js plan

---
+++ js/main.custom-0.1.js
@@ -0,0 +1,32 @@
+// add click listener on config cog icon
+// cog opens config tray - slide in from left to right
+// config options: use custom player (default) or mediaElement player
+
+// if mediaElement is selected append the required classes and script to the DOM
+//   -- using the appended classes control media playback and customize the skin
+//   -- then run code from the additional features section below which will have a case for if mediaElement
+// else run functions below for my custom media player:
+
+//////////////////////////////////////////////////////////////
+// *** PLAYER MUST FUNCTION IN AT LEAST 3 MODERN BROWSERS ***
+/////////////////////////////////////////////////////////////
+
+
+// Elements needed:
+
+// closed caption (default)
+// play
+// pause
+// track start time
+// back n seconds
+// current time/progress bar
+// track end time
+// sound
+// fullscreen
+
+
+
+// additional features:
+
+// current time in video appends highlights on the relevant text on page
+// clicking on the text skips to the appropriate time
