3107cf01e42dbba6b082422a8f72ad14be46eba0
Warren Leyes <warren.leyes@gmail.com>
2018-02-20 16:16:02 -0800

    temp placed controls back on video player. further flushed out js plan.

--- js/main.custom-0.1.js
+++ js/main.custom-0.1.js
@@ -10,12 +10,27 @@
 //////////////////////////////////////////////////////////////
 // *** PLAYER MUST FUNCTION IN AT LEAST 3 MODERN BROWSERS ***
 /////////////////////////////////////////////////////////////
+// Target browsers:
+// Chrome
+// Firefox
+// Opera
+
+// if I have time:
+// Safari
+// MS Edge/IE
 
 
 // Elements needed:
 
+// onload: check if browser can play type
+//  thinking of setting the controls in the html(as a fallback) then disable via js | video.controls=false
+// -- if can play type mp4 then ...
+// -- if can play type ogg then ...
+// -- else error
+
 // Controls:
 // on mobile stick to bottom (maybe scroll and highlite bottom offscreen up to the player? todo: figureout how to make the most out of mobile screen size)
+
 //  - closed caption (default)
 //  - play
 //  - pause
