fd180a8531f2c1b3b024586ada32287864eeb00d
Warren Leyes <warren.leyes@gmail.com>
2018-02-23 14:08:53 -0800

    finished play/pause functionality

--- js/main.custom-0.1.js
+++ js/main.custom-0.1.js
@@ -1,11 +1,12 @@
  window.addEventListener("load", function(event) {
-  const videoContainer = document.querySelector('.video__player--container');
-  const video          = document.querySelector('.video__player');
-  video.controls       = false; // todo: if html5 video is supported, only add my controls if javascript is enabled
+  const videoContainer  = document.querySelector('.video__player--container');
+  const video           = document.querySelector('.video__player');
+  video.controls        = false; // todo: if html5 video is supported, only add my controls if javascript is enabled
 
-  const videoControls  = document.querySelector('.video__player--controls');
-  const play        = document.querySelector('.video__player__controls--play');
-  const playClasses    = play.classList;
+  const videoControls   = document.querySelector('.video__player--controls');
+  const play            = document.querySelector('.video__player__controls--play');
+  let playPauseToggle   = document.querySelector('.fa-play');
+  const playClasses     = play.classList;
 
    play.addEventListener('click', function () {
     if (video.ended) {
@@ -18,25 +19,26 @@
     }
   }, false);
 
-// half functional in Chrome Opera and Firefox as it changes from pause state to play and switches icon from fa-play to fa-pause
-// in MS Edge play and pause functions correctly but icon keeps same state (fa-play) likley due to the replace() i am using for now
-
   video.addEventListener('play', function () {
+    playPauseToggle = document.querySelector('.fa-play');
+    playPauseToggle.classList.remove('fa-play');
+    playPauseToggle.classList.add('fa-pause');
     play.title = 'pause';
-    play.classList.replace('fa-play', 'fa-pause');
-    alert('from pause state to play!'); // test
   }, false);
 
   video.addEventListener('pause', function () {
+    playPauseToggle = document.querySelector('.fa-pause');
+    playPauseToggle.classList.remove('fa-pause');
+    playPauseToggle.classList.add('fa-play');
     play.title = 'play';
-    play.classList.replace('fa-pause', 'fa-play');
-    alert('from play state to pause!'); // test not triggering on click but is triggered on video.ended
   }, false);
 
 
   video.addEventListener('ended', function () {
     this.pause();
-    play.classList.replace('fa-pause', 'fa-play');
+    playPauseToggle = document.querySelector('.fa-pause');
+    playPauseToggle.classList.remove('fa-pause');
+    playPauseToggle.classList.add('fa-play');
   }, false);
 
 
