e135c904b4d00dffab441f7f62d448271444a1b3
Warren Leyes <warren.leyes@gmail.com>
2018-02-23 11:55:02 -0800

    forgot to load the document Doh!

--- js/main.custom-0.1.js
+++ js/main.custom-0.1.js
@@ -1,36 +1,40 @@
-const videoContainer   = document.querySelector('.video__player--container');
-const video            = document.querySelector('.video__player'); // why is this coming back null? I am able to dispalay in console!
-video.controls         = false; // todo: if html5 video is supported only add my controls if javascript is enabled
-const videoControls    = document.querySelector('.video__player--controls');
-const playBtn          = document.querySelector('.video__player__controls--play');
-const playClasses    = play.classList;
+ window.addEventListener("load", function(event) {
+  const videoContainer = document.querySelector('.video__player--container');
+  const video          = document.querySelector('.video__player'); // why is this coming back null? I am able to dispalay in console!
 
- playBtn.addEventListener('click', function () {
-  if (video.ended) {
-    video.currentTime = 0;
-  }
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-}, false);
+  video.controls       = false; // todo: if html5 video is supported, only add my controls if javascript is enabled
 
+  const videoControls  = document.querySelector('.video__player--controls');
+  const play        = document.querySelector('.video__player__controls--play');
+  const playClasses    = play.classList;
 
-video.addEventListener('play', function () {
-  play.classList.remove('fa-play');
-  play.classList.add('fa-pause');
-}, false);
+   play.addEventListener('click', function () {
+    if (video.ended) {
+      video.currentTime = 0;
+    }
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }, false);
 
 
-video.addEventListener('pause', function () {
-  play.classList.remove('fa-pause');
-  play.classList.add('fa-play');
-}, false);
+  video.addEventListener('play', function () {
+    play.classList.remove('fa-play');
+    play.classList.add('fa-pause');
+  }, false);
 
 
-video.addEventListener('ended', function () {
-  this.pause();
-  play.classList.remove('fa-pause');
-  play.classList.add('fa-play');
-}, false);
+  video.addEventListener('pause', function () {
+    play.classList.remove('fa-pause');
+    play.classList.add('fa-play');
+  }, false);
+
+
+  video.addEventListener('ended', function () {
+    this.pause();
+    play.classList.remove('fa-pause');
+    play.classList.add('fa-play');
+  }, false);
+});
