c860489dbfe3872819654fbdc802aca8eaa306da
Warren Leyes <warren.leyes@gmail.com>
2018-02-23 11:08:11 -0800

    quick notes added

--- js/main.custom-0.1.js
+++ js/main.custom-0.1.js
@@ -1,6 +1,6 @@
 const videoContainer   = document.querySelector('.video__player--container');
-const video            = document.querySelector('.video__player');
-supportsVideo.controls = false; // todo: if html5 video is supported only add my controls if javascript is enabled
+const video            = document.querySelector('.video__player'); // why is this coming back null? I am able to dispalay in console!
+video.controls = false; // todo: if html5 video is supported only add my controls if javascript is enabled
 const videoControls    = document.querySelector('.video__player--controls');
 const play             = document.querySelector('.video__player__controls--play');
 
