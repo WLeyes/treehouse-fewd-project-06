3c43f75ee8461f85871d69add69608cd1f57a604
Warren Leyes <warren.leyes@gmail.com>
2018-02-23 11:23:48 -0800

    pulling out last of my hair!!

--- js/main.custom-0.1.js
+++ js/main.custom-0.1.js
@@ -1,11 +1,11 @@
 const videoContainer   = document.querySelector('.video__player--container');
 const video            = document.querySelector('.video__player'); // why is this coming back null? I am able to dispalay in console!
-video.controls = false; // todo: if html5 video is supported only add my controls if javascript is enabled
+video.controls         = false; // todo: if html5 video is supported only add my controls if javascript is enabled
 const videoControls    = document.querySelector('.video__player--controls');
-const play             = document.querySelector('.video__player__controls--play');
+const playBtn          = document.querySelector('.video__player__controls--play');
+const playClasses    = play.classList;
 
- // const playClasses    = play.classList;
- play.addEventListener('click', function () {
+ playBtn.addEventListener('click', function () {
   if (video.ended) {
     video.currentTime = 0;
   }
@@ -16,15 +16,18 @@ const play             = document.querySelector('.video__player__controls--play'
   }
 }, false);
 
+
 video.addEventListener('play', function () {
   play.classList.remove('fa-play');
   play.classList.add('fa-pause');
 }, false);
 
+
 video.addEventListener('pause', function () {
   play.classList.remove('fa-pause');
   play.classList.add('fa-play');
 }, false);
+
 
 video.addEventListener('ended', function () {
   this.pause();
