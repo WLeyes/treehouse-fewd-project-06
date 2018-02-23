596ae57a9e07977a72334ee94ae31b13e234a18c
Warren Leyes <warren.leyes@gmail.com>
2018-02-23 10:26:21 -0800

    reworking the playfunction in js,

--- js/main.custom-0.1.js
+++ js/main.custom-0.1.js
@@ -1,113 +1,41 @@
-// add click listener on config cog icon
-// cog opens config tray - slide in from left to right
-// config options: use custom player (default) or mediaElement player
+const supportsVideo = !!document.createElement('video').canPlayType;
+if (supportsVideo) {
+ // alert('yes');
+ const video          = document.querySelector('.video__player');
+ const videoContainer = document.querySelector('.video__player--container');
+ const videoControls  = document.querySelector('.video__player--controls');
+ const play           =  document.querySelector('.video__controls--play');
+ const playClasses    = play.classList;
 
-// if mediaElement is selected append the required classes and script to the DOM
-//   -- using the appended classes control media playback and customize the skin
-//   -- then run code from the additional features section below which will have a case for if mediaElement
-// else run functions below for my custom media player:
+ play.addEventListener('click', function () {
+  if (video.ended) {
+    video.currentTime = 0;
+  }
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}, false);
 
-//////////////////////////////////////////////////////////////
-// *** PLAYER MUST FUNCTION IN AT LEAST 3 MODERN BROWSERS ***
-/////////////////////////////////////////////////////////////
-// Target browsers:
-// Chrome
-// Firefox
-// Opera
+video.addEventListener('play', function () {
+  play.title = 'pause';
+  play.classList.remove('fa-play');
+  play.classList.add('fa-pause');
+}, false);
 
-// if I have time:
-// Safari
-// MS Edge/IE
+video.addEventListener('pause', function () {
+  play.title = 'play';
+  play.classList.remove('fa-pause');
+  play.classList.add('fa-play');
+}, false);
 
+video.addEventListener('ended', function () {
+  this.pause();
+  play.classList.remove('fa-pause');
+  play.classList.add('fa-play');
+}, false);
 
-// Elements needed:
-
-// onload: check if browser can play type
-//  thinking of setting the controls in the html(as a fallback) then disable via js | video.controls=false
-// -- if can play type mp4 then ...
-// -- if can play type ogg then ...
-// -- else error
-
-// Controls:
-// on mobile stick to bottom (maybe scroll and highlite bottom offscreen up to the player? todo: figureout how to make the most out of mobile screen size)
-
-//  - closed caption (default)
-//  - play
-//  - pause
-//  - track start time
-//  - back n seconds
-//  - current time/progress bar
-//  - track end time
-//  - sound
-//  - fullscreen
-
-function videoPlayer() {
-  controls = {
-    cc:          $('<i class="fal fa-closed-captioning"></i>'),
-    play:        $('<i class="fal fa-play-circle"></i>'),
-    pause:       $('<i class="fal fa-pause-circle"></i>'),
-    startTime:   $(''),
-    skipBack:    $(''),
-    currentTime: $(''),
-    endTime:     $(''),
-    volume-up:   $('<i class="fal fa-volume-up"></i>'),
-    volume-down: $('<i class="fal fa-volume-down"></i>'),
-    volume-mute: $('<i class="fal fa-volume-mute"></i>'),
-    fullscreen:  $('<i class="fal fa-expand-wide"></i>')
-  };
-}
-
-// Closed Caption function
-function cc() {
-  // closed caption is on by default, set cc status to on and set color to green when on
-    // if cc is on and clicked, turn cc off
-    // else  turn cc on
-}
-
-// Play and pause function
-function play(){
-  // set default icon to the play status play/paused
-  // if the video is not playing and the play button is pressed, play the video
-    // else if the video is playing, puse the vido and set icon to pause
-}
-
-// play time functions
-
-function playStartTime() {
-  // set play start time 0
-}
-
-function currentPlayPosition() {
-  // get the current playback time poision
-}
-
-function playEndTime() {
-  // get the playback end time
-}
-
-
-// Volume control functions
-function volume() {
-  // turn volume up
-  //turn volume down
-  // mute
-}
-
-function fullscreen() {
-  // check if fullscreen
-  // if not full screen, make fullscreen
-    // set notice on how to exit fullscreen
-}
-
-
-// additional features:
-
-// current time in video appends highlights on the relevant text on page
-function highlight() {
-  // if current play position is within time range, then highlight the text for that section
-}
-
-// clicking on the text skips to the appropriate time
-function jumpToPossition() {
-  // onclick of text set current time position to that section in the video and skip to that part of the video
+} else {
+  alert('no');
 }
