410d41074c5159a9f75a4dc2d5c834a3bb0170df
Warren Leyes <warren.leyes@gmail.com>
2018-02-23 22:07:13 -0800

    Cleaned up markup, css and js to make easier to read

--- js/main.custom-0.1.js
+++ js/main.custom-0.1.js
@@ -1,20 +1,20 @@
  window.addEventListener("load", function(event) {
 
  // Video player
-  const videoContainer  = document.querySelector('.video__player--container');
-  const video           = document.querySelector('.video__player');
+  const videoContainer  = document.querySelector('.player--container');
+  const video           = document.querySelector('.player');
   video.controls        = false; // todo: if html5 video is supported, only add my controls if javascript is enabled
 
 // Video player controls
-  const videoControls   = document.querySelector('.video__player--controls');
-  const play            = document.querySelector('.video__player__controls--play');
+  const videoControls   = document.querySelector('.player__controls');
+  const play            = document.querySelector('.player__controls--play');
   let playPauseToggle   = document.querySelector('.fa-play');
 
 //Fullscreen
   let fullScreenEnabled = !!(document.fullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitSupportsFullscreen || document.webkitFullscreenEnabled || document.createElement('video').webkitRequestFullScreen);
-  const fullscreen      = document.querySelector('.video__player__controls--fullscreen');
+  const fullscreen      = document.querySelector('.player__controls--fullscreen');
 
- 
+
 // play and pause button controls
    play.addEventListener('click', function () {
     if (video.ended) {
