380afdcd2c1e1688c25fd7572d79640b274835b2
Warren Leyes <warren.leyes@gmail.com>
2018-02-27 00:11:33 -0800

    fixed css and html errors, started with highlight function and cc

--- js/main.custom-0.1.js
+++ js/main.custom-0.1.js
@@ -153,6 +153,25 @@ function closedCaption() {
   });
 }
 
+function captionHighlight() {
+  const video = document.querySelector('.player');
+  const span = document.querySelectorAll('span');
+  video.addEventListener('timeupdate', () => {
+    for (let i = 0; i < span.length; i+= 1) {
+      if(video.currentTime > span[i].getAttribute('data-start') && video.currentTime < span[i].getAttribute('data-end')) {
+        span[i].style.backgroundColor='yellow';
+      } else {
+        span[i].style.backgroundColor='';
+      }
+    }
+  });
+  for (i = 0; i < span.length; i += 1) {
+      span[i].addEventListener('click', (event) =>  {
+        video.currentTime = event.target.getAttribute('data-start');
+      })
+    }
+  } // end captionHighlight
+
 // Volume slider
 function volume() {
   const video      = document.querySelector('.player');
